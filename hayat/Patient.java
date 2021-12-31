038d152495c755de4f3cc136e65b232569332999
mariamazzam18 <mariam__azzam@hotmail.com>
2021-12-31 23:03:25 +0300

    add new

--- hayat/Patient.java
+++ hayat/Patient.java
@@ -1,39 +1,77 @@
-/*
-PROJECT
-SECTION : 01
-STUDENT 1:
-       Name : Gehad Elsayed 
-       ID : 20176281
-STUDENT 2 : 
-       Name : 
-       ID : 
-*/
-Import java.util.ArrayList ;
-Public class HYAT
-{
-private String cpr;              //ID number of the student
-private String Fname;       // first name of the student
-private String Iname;        //last name of the student
-private String gender;      // gender of the student
-private String email;        // email of the student
-private String bloodType;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
+public class Patient{
+    public static void main(String[] args){
 
-// constructor to initialize the data members depending on the parameters that are passed to the method
-public Patient (String cpr, String fame, String lname, String gender, String email, String bloodType) 
-{
-this. cpr = cpr;
-Fame = fname;
-Lname = lname
-this. gender = gender;
-this. email = email:
-this. bloodType= bloodType;
-}
-// default constructor to initialize the data members to default values 
-Public Patient () { 
-cpr=“”;
-Fname=“”;
-Lname=“”;
-gender=“”;
-email=“”;
-bloodType=“”
+        Scanner kbd = new Scanner(System.in);
+        boolean flag = true;
+
+        ArrayList<Process> list = new ArrayList<Process>();
+        PlzWork s = new PlzWork();
+        int atime , pri , burst ;
+        String pid;
+      /*  System.out.println("All zeros to exit: ");
+        System.out.println("Enter process: ");
+        pid= kbd.next();
+        System.out.println("Enter Arrival time: ");
+        atime = kbd.nextInt();
+        System.out.println("Enter Priority: ");
+        pri = kbd.nextInt();
+        System.out.println("Enter Burst time: ");
+        burst = kbd.nextInt();
+
+        while(pid != "0" && atime != 0 && pri != 0 && burst != 0)
+        {
+
+            list.add( new Process(pid, atime, pri, burst));
+        System.out.println("All zeros to exit: ");
+        System.out.println("Enter process: ");
+        pid= kbd.next();
+        System.out.println("Enter Arrival time: ");
+        atime = kbd.nextInt();
+        System.out.println("Enter Priority: ");
+        pri = kbd.nextInt();
+        System.out.println("Enter Burst time: ");
+        burst = kbd.nextInt();
+
+
+
+        } */
+        ArrayList<Process> list1 = new ArrayList<Process>();
+        list1.add(new Process("p1", 0, 2, 11));
+        list1.add(new Process("p2", 5, 0, 28));
+        list1.add(new Process("p3", 12, 3, 2));
+        list1.add(new Process("p4", 2, 1, 10));
+        list1.add(new Process("p5", 9, 4, 16));
+
+
+
+
+        ArrayList<Process> test = new ArrayList<Process>();
+        Collections.sort(list1);
+        test = s.Gantt(list1);
+
+        for(int i = 0 ; i< test.size() ; i++) {
+
+            System.out.println(i + "-" + test.get(i).print());
+            System.out.println();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
---
+++ hayat/Process.java
@@ -0,0 +1,46 @@
+class Process implements Comparable<Process>  {
+    private String pid;
+    private int atime;
+    private   int priority;
+    private int burst;
+
+    public Process(String P, int A ,int Pr ,int B){
+        this.pid = P;
+        this.atime = A;
+        this.priority = Pr;
+        this.burst = B;
+    }
+
+    public int getAtime() {
+        return atime;
+    }
+
+
+    public int getBurst() {
+        return burst;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public String getPid() {
+        return pid;
+    }
+
+    public void setBurst(int burst) {
+        this.burst = burst;
+    }
+
+    public String print(){
+
+        return "Pid: " + getPid() + "    Arrival time:   "+ getAtime() + "   Priority: "+ getPriority() + "   Burst time: "+ getBurst();
+    }
+
+
+    @Override
+    public int compareTo(Process pid) {
+        int compareArrival = ((Process)pid).getAtime();
+        return this.atime- compareArrival;
+    }
+}
